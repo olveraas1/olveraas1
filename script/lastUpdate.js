595c75ff233fabc4c33177d63c7c8773cc11329b
Use your GitHub olveraas <Enter your email address olveraas@gmail.com>
2020-09-19 11:08:11 -0600

    update

--- script/lastUpdate.js
+++ script/lastUpdate.js
@@ -2,6 +2,6 @@
 //     var x = document.lastModified;
 //     document.getElementById("demo").innerHTML = x;
 //   }
-document.write("java: " + document.lastModified +""); 
+document.write("LastUpdate: " + document.lastModified +""); 
 
 // http://www.web-source.net/web_design_tips/last_modified_date_web_page_script.htm#.XqCiafl7lEY
