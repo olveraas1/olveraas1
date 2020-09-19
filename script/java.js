c2b8cdeaec1cebb0c94ddd9b048cf10db0a42d1e
Use your GitHub olveraas <Enter your email address olveraas@gmail.com>
2020-09-19 09:48:58 -0600

    initial commit

---
+++ script/java.js
@@ -0,0 +1,7 @@
+// function myFunction() {
+//     var x = document.lastModified;
+//     document.getElementById("demo").innerHTML = x;
+//   }
+document.write("java: " + document.lastModified +""); 
+
+// http://www.web-source.net/web_design_tips/last_modified_date_web_page_script.htm#.XqCiafl7lEY
